3da797980274a0c901ee2f147a46cd0fab93ce15
yinrongjie <250371746@qq.com>
2020-12-13 20:12:46 +0800

    1. 优化了日志打印 2. 增加eureka的配置

--- eureka/src/main/java/com/yinrj/EurekaApplication.java
+++ eureka/src/main/java/com/yinrj/EurekaApplication.java
@@ -1,15 +1,24 @@
 package com.yinrj;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 /**
  * @author Yin
  * @date 2020/12/13
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class);
+        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOGGER.info("启动成功");
+        LOGGER.info("Eureka地址: \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 }
