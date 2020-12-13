2d3d2d8bca81ec914301b13a63a620969a53a346
yinrongjie <250371746@qq.com>
2020-12-13 19:45:04 +0800

    项目初始化 1. 新建父工程 2. 新建了eureka工程

---
+++ eureka/src/main/java/com/yinrj/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.yinrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yin
+ * @date 2020/12/13
+ */
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
