cea4dedb5332d8b8609c866a9b7bd6f7f79e3986
yinrongjie <250371746@qq.com>
2020-12-19 22:38:43 +0800

    1. 新建business模块 2. 生成大章相关的持久层代码

---
+++ business/src/main/java/com/yinrj/business/BusinessApplication.java
@@ -0,0 +1,26 @@
+package com.yinrj.business;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author Yin
+ * @date 2020/12/19
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.yinrj.server.mapper")
+public class BusinessApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BusinessApplication.class);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOGGER.info("启动成功");
+        LOGGER.info("Business地址: \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+}
