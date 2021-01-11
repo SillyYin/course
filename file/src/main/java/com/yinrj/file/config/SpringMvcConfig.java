117a821b4f60e4d148fb9b87f4c647d222a3e979
YinRJ <yin_rj@qq.com>
2021-01-11 22:28:58 +0800

    文件路径可配置

--- file/src/main/java/com/yinrj/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/yinrj/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.yinrj.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,12 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Value("${file.path}")
+    private String FILE_PATH;
     /**
      * 增加静态资源的配置
      * @param registry
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:/Users/yinrongjie/Desktop/file/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
     }
 }
--- file/src/main/java/com/yinrj/file/controller/admin/UploadController.java
+++ file/src/main/java/com/yinrj/file/controller/admin/UploadController.java
@@ -3,6 +3,7 @@ package com.yinrj.file.controller.admin;
 import com.yinrj.server.dto.ResponseDto;
 import com.yinrj.server.util.UuidUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,23 +21,31 @@ import java.io.IOException;
 @RequestMapping("/admin")
 @Slf4j
 public class UploadController {
-    private static final String PRE_PATH = "/Users/yinrongjie/Desktop/file/";
-    private static final String BUSINESS_NAME = "文件上传";
+
+    public static final String BUSINESS_NAME = "文件上传";
+
+    @Value("${file.domain}")
+    private String FILE_DOMAIN;
+
+    @Value("${file.path}")
+    private String FILE_PATH;
 
     @PostMapping("/upload")
-    public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
+    public ResponseDto<String> upload(@RequestParam MultipartFile file) throws IOException {
         log.info("文件开始上传: {}", file);
         log.info(file.getOriginalFilename());
         log.info(String.valueOf(file.getSize()));
 
         String fileName = file.getOriginalFilename();;
         String key = UuidUtil.getShortUuid();
-        String fullPath = PRE_PATH + key + "-" + fileName;
+        String fullPath = FILE_PATH + key + "-" + fileName;
+        log.info("文件全路径：{}", fullPath);
         File dest = new File(fullPath);
         file.transferTo(dest);
         log.info(dest.getAbsolutePath());
 
-        ResponseDto responseDto = new ResponseDto();
+        ResponseDto<String> responseDto = new ResponseDto<>();
+        responseDto.setContent(FILE_DOMAIN + key + "-" + fileName);
         return responseDto;
     }
 }
